c5a1bfb4c4fe3d90555d0c2f36b4045287379cc7
kaurakeksi <ossi.hirvola@gmail.com>
2014-11-24 17:50:45 +0200

    Primitive minimap added

---
+++ QuadcopterDemo/Assets/Minimap.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimap : MonoBehaviour {
+	
+	public Transform Target;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (Target.position.x, transform.position.y, Target.position.z);
+	}
+}
