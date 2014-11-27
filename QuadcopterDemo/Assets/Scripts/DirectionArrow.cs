c49a149250c9aa62100b608b2f754601e3451e43
Miro Vilkki <miro.vilkki@aalto.fi>
2014-11-27 15:52:26 +0200

    Improved camera

--- QuadcopterDemo/Assets/Scripts/DirectionArrow.cs
+++ QuadcopterDemo/Assets/Scripts/DirectionArrow.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class DirectionArrow : MonoBehaviour {
 
 	public Transform target;
-	
+    private float distance;
+
 	void Update () {
         if (!target)
         {
@@ -18,4 +19,28 @@ public class DirectionArrow : MonoBehaviour {
         }
 		
 	}
+
+    void OnGUI()
+    {
+        if (!target)
+            return;
+        const float width = 100;
+        const float height = 20;
+
+        GUIStyle style = GUI.skin.label;
+        style.fontSize = 16;
+        style.normal.textColor = new Color(1, 1, 1);
+        style.hover.textColor = style.normal.textColor * 0.8f;
+        style.active.textColor = style.normal.textColor * 0.6f;
+        style.fontStyle = FontStyle.Bold;
+        style.alignment = TextAnchor.MiddleCenter;
+
+        GUI.backgroundColor = Color.clear;
+
+        float distance = (target.position - transform.position).magnitude;
+        if (renderer.enabled)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - width / 2, Screen.height * 0.09f, width, height), distance.ToString("#.0") + " m");
+        }
+    }
 }
--- QuadcopterDemo/Assets/Scripts/ThirdPersonCamera.cs
+++ QuadcopterDemo/Assets/Scripts/ThirdPersonCamera.cs
@@ -7,20 +7,36 @@ public class ThirdPersonCamera : MonoBehaviour {
     public float distanceUp;
     public float distanceAway;
     public float smooth;
+    public float rotationSpeed;
+    public float mouseSensitivity;
+    public float minOffsetDistance;
 
     private Vector3 targetPosition;
     private Vector3 lookDirection;
     private Vector3 velocityCamSmooth = Vector3.zero;
     private float camSmoothDampTime = 0.1f;
-	
-	void LateUpdate () {
+    private GameLogic gameLogic;
+
+    void Start()
+    {
+        gameLogic = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<GameLogic>();
+    }
+
+    void LateUpdate()
+    {
+        if (gameLogic.gameState == GameState.IN_MENU)
+            return;
+        float mouseInput = Input.GetKey(KeyCode.Mouse1) ? mouseSensitivity * Input.GetAxis("Mouse X") : 0;
+        float rotationInput = Input.GetAxis("Horizontal2") != 0 ? Input.GetAxis("Horizontal2") : mouseInput;
+        transform.RotateAround(target.position, Vector3.up, Time.deltaTime * rotationSpeed * rotationInput);
+
         Vector3 characterOffset = target.position + new Vector3(0, distanceUp, 0);
         
         lookDirection = characterOffset - this.transform.position;
         lookDirection.y = 0;
         lookDirection.Normalize();
 
-        targetPosition = characterOffset + target.up * distanceUp - lookDirection * distanceAway;
+        targetPosition = characterOffset - lookDirection * distanceAway;
 
         CheckCollision(characterOffset, ref targetPosition);
         SmoothPosition(this.transform.position, targetPosition);
@@ -36,11 +52,42 @@ public class ThirdPersonCamera : MonoBehaviour {
 
     private void CheckCollision(Vector3 fromObject, ref Vector3 toTarget)
     {
-        RaycastHit hit = new RaycastHit();
-        if (Physics.Linecast(fromObject, toTarget, out hit))
+        float offsetDistance = (fromObject - toTarget).magnitude;
+        float raycastLength = offsetDistance - minOffsetDistance;
+
+        if (raycastLength < 0.0f)
+            return;
+
+        Vector3 cameraOut = (fromObject - toTarget).normalized;
+        Vector3 nearestCameraPosition = fromObject - cameraOut * minOffsetDistance;
+        float minHitFraction = 1.0f;
+
+        Vector3[] corners = GetNearPlaneCorners(toTarget);
+        for (int i = 0; i < 4; i++)
         {
-            //Vector3 offset = new Vector3(fromObject.x - hit.point.x, 0, fromObject.z - hit.point.z).normalized * 0.5f;
-            toTarget = new Vector3(hit.point.x, toTarget.y, hit.point.z);
+            Vector3 cornerOffset = corners[i] - toTarget;
+            Vector3 rayStart = nearestCameraPosition + cornerOffset;
+            Vector3 rayEnd = corners[i];
+
+            RaycastHit hit = new RaycastHit();
+            Debug.DrawRay(rayStart, rayEnd - rayStart);
+            if (Physics.Linecast(rayStart, rayEnd, out hit))
+                minHitFraction = hit.distance / raycastLength;
         }
+
+        if (minHitFraction < 1.0f)
+            toTarget = nearestCameraPosition - cameraOut * Mathf.Min(raycastLength * minHitFraction * 0.99f, 0);
+    }
+
+    private Vector3[] GetNearPlaneCorners(Vector3 toTarget)
+    {
+        Vector3[] corners = new Vector3[4];
+        float offsetX = camera.nearClipPlane * Mathf.Tan(camera.fieldOfView / 2);
+        float offsetY = camera.nearClipPlane * Mathf.Tan(camera.fieldOfView / 2);
+        corners[0] = toTarget + new Vector3(-offsetX, offsetY, camera.nearClipPlane);
+        corners[1] = toTarget + new Vector3(-offsetX, -offsetY, camera.nearClipPlane);
+        corners[2] = toTarget + new Vector3(offsetX, -offsetY, camera.nearClipPlane);
+        corners[3] = toTarget + new Vector3(offsetX, offsetY, camera.nearClipPlane);
+        return corners;
     }
 }
